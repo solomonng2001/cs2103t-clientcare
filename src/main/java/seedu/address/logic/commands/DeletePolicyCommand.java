f18b459586dc200857fae97d6cb93a129eb08a65
solomonng2001 <solomonng2001@gmail.com>
2024-04-12 13:19:45 +0800

    fix-checkstyle

--- src/main/java/seedu/address/logic/commands/DeletePolicyCommand.java
+++ src/main/java/seedu/address/logic/commands/DeletePolicyCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICYID;
-import static seedu.address.model.Model.COMPARATOR_SHOW_ORIGINAL_ORDER;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
